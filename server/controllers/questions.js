230ca32e1258d1d3c177d2a7c57ec9358605f4c4
mml1 <mmlliguicota@gmail.com>
2016-04-30 16:05:31 -0700

    added controller info

--- server/controllers/questions.js
+++ server/controllers/questions.js
@@ -1,24 +1,24 @@
 var mongoose = require("mongoose");
-var Factor = mongoose.model("Factor");
+var Question = mongoose.model("Question");
 //immediate function
 module.exports = (function(){
-	return {
-		index: function(req, res){
-			Factor.find({}, function(err, factors){
-				if(err){
-					console.log("db is empty")
-				} else {
-					res.json(factors);
-				}
-			});
+    return {
+        index: function(req, res){
+        Question.where('category ').in(['b',req.body.category]).exec(function(req,questions){
+            if (err) {
+                console.log('get questions errors',err)
+            }else{
+                res.json(questions);
+            }
+        })
 
-		}, 
-		create: function(req, res){
+        }
+        // create: function(req, res){
 
-		}, 
-		delete: function(req, res){
-			
-		}
+        // }, 
+        // delete: function(req, res){
+            
+        // }
 
-	}
+    }
 })();
