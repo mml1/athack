ad81d7d1835b69a5d6cdbb54937005aebb83dafd
Anna Propas <apropas@gmail.com>
2016-04-30 14:59:56 -0700

    initial commit, changes to db and schema

--- server/models/factor.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose");
-var Schema = mongoose.Schema;
-var FactorSchema = new mongoose.Schema({
-	// name:String, 
-	// created_at: {type: Date, default: new Date},
-	// // orders: [{type: Schema.Types.ObjectId, ref: "Order"}]
-});
-mongoose.model("Factor", FactorSchema);
---
+++ server/models/question.js
@@ -0,0 +1,10 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+var QuestionSchema = new mongoose.Schema({
+	text: String,
+	risk_score: Number,
+	keyword: String,
+	category: String,
+	created_at: {type: Date, default: new Date}
+});
+mongoose.model("Question", QuestionSchema);
