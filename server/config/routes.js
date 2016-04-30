57984a17ce1c9fd9865251113256c0436be8a896
mml1 <mmlliguicota@gmail.com>
2016-04-30 15:59:52 -0700

    fixed route

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,4 +1,4 @@
-var factors = require("./../controllers/factors.js");
+var factors = require("./../controllers/questions.js");
 
 module.exports = function(app){
 	app.get("/REPLACE", function(req, res){
