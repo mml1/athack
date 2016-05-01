b3ffa3b9bedfd9490b3d866a06137712a83b8b13
mml1 <mmlliguicota@gmail.com>
2016-05-01 13:15:50 -0700

    fixed db mongoose file

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 var models_path = __dirname + "/../models";
 
 //connecting to my database
-mongoose.connect("mongodb://localhost/warningSigns");
+mongoose.connect("mongodb://localhost/warningSignsDB");
 
 //my db will have two collections
 fs.readdirSync(models_path).forEach(function(file){
